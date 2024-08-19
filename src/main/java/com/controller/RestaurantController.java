5f57bda193ad04dad941f39ef4e76b65af8edb92
Khush-Mevada21 <khushmevada204@gmail.com>
2024-08-19 20:24:51 +0530

    list

--- src/main/java/com/controller/RestaurantController.java
+++ src/main/java/com/controller/RestaurantController.java
@@ -1,12 +1,18 @@
 package com.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.entity.RestaurantEntity;
 import com.repository.RestaurantRepository;
+
+
 
 @Controller
 public class RestaurantController {
@@ -26,5 +32,16 @@ public class RestaurantController {
 		restaurantRepository.save(restaurantEntity);
 		
 		return "Success";
+	}
+	
+	@GetMapping("/listrestaurants")
+	public String listRestaurants(Model model)
+	{
+		List<RestaurantEntity> restaurants = restaurantRepository.findAll();
+		
+		model.addAttribute("restaurants",restaurants);
+		
+		//restaurantRepository.deleteById(restaurantId);
+		return "ListRestaurant";
 	}
 }
