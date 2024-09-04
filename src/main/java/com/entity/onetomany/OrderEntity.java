cce010c28d6500370980c0570cb9e12ba49173c8
Khush-Mevada21 <khushmevada204@gmail.com>
2024-09-04 19:21:02 +0530

    one to many entity

---
+++ src/main/java/com/entity/onetomany/OrderEntity.java
@@ -0,0 +1,30 @@
+package com.entity.onetomany;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name = "orders")
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class OrderEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer orderId;
+	String orderDate;
+	Float amount;
+
+	@ManyToOne
+	@JoinColumn(name="customerId")
+	CustomerEntity customer;
+}
